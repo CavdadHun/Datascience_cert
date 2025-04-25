cfe41248eab866546d467d1cc05e8323afcb0f1c
CavdadHun <becz.viktor@gmail.com>
2025-04-25 07:23:31 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+# This is a simple arithmetic expression to multiply then add integers.
+result = (3 * 4) + 5
+print(result)
+
+# This will convert 200 minutes to hours by dividing by 60
+hours = 200 / 60
+print(hours)
